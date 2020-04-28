0156a7bf163a3674ea8abff1a438dde63a58b382
AidanJK <aidan.j.kaufman@gmail.com>
2020-04-28 15:00:57 -0400

    Adding an app.use for the about.html page

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/index.html`));
 });
 
-app.set("/about.html", path.resolve(`${__dirname}/about.html`));
+app.use("/about.html", path.resolve(`${__dirname}/about.html`));
 
 const server = http.createServer(app);
 
