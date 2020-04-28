75c65c00e226ca95893c77c6aa7c76abf16d2a3e
AidanJK <aidan.j.kaufman@gmail.com>
2020-04-28 14:01:33 -0400

    Testing out sending more than just the index.html file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 // Credit to simple-server-collision by Cody Van De Mark
-const http = require('http');
-const path = require('path');
-const express = require('express');
+const http = require("http");
+const path = require("path");
+const express = require("express");
 
 const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/index.html`));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}`));
 });
 
 const server = http.createServer(app);
