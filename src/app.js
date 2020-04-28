8a785cb35feffbba45e9efa68679beb0e69b54cf
AidanJK <aidan.j.kaufman@gmail.com>
2020-04-28 15:24:11 -0400

    Adding app.use before app.get

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const app = express();
 
+app.use("/about", express.static(path.resolve(`${__dirname}/about.html`)));
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/index.html`));
 });
 
-// app.use("/about.html", path.resolve(`${__dirname}/about.html`));
+// app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 
 const server = http.createServer(app);
 
